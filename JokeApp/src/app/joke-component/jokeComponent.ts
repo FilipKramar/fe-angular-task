0e96983d7c535300131b078382e4985904b424da
FilipKramar <filip.kramar4637@gmail.com>
2024-02-16 16:53:01 +0100

    Added favourite jokes

--- JokeApp/src/app/joke-component/jokeComponent.ts
+++ JokeApp/src/app/joke-component/jokeComponent.ts
@@ -7,7 +7,7 @@ import { JokeService } from '../joke-service';
   styleUrls: ['./jokeComponent.css']
 })
 export class JokeComponent implements OnInit {
-
+  favouriteJokes:any[]=[];
   jokes:any[]=[];
   currentIndex=0;
   isClicked:boolean=false;
@@ -16,15 +16,17 @@ export class JokeComponent implements OnInit {
  
   ngOnInit(): void {
     this.loadJokes();
+    this.loadFavoriteJokes();
    
  }
 
- toggleHeart(): void {
-  this.isClicked = !this.isClicked;
-}
 
 loadNextJoke() {
   this.currentIndex = (this.currentIndex + 1);
+  if(this.favouriteJokes.includes(this.jokes[this.currentIndex])){
+    this.isClicked=true;
+  }
+  else this.isClicked=false;
   if(this.currentIndex>9){
     this.loadJokes();
   }
@@ -34,5 +36,33 @@ loadJokes() {
     this.jokes = data.jokes; 
     this.currentIndex = 0; 
   });
+}
+
+toggleFavorite() {
+  const currentJoke = this.jokes[this.currentIndex];
+  this.isClicked = !this.isClicked;
+  if (this.isClicked) {
+    const jokeWithTimestamp = {
+      joke: currentJoke,
+      timestamp: new Date().toDateString()
+    };
+    this.favouriteJokes.push(jokeWithTimestamp);
+  } else {
+    this.favouriteJokes = this.favouriteJokes.filter(joke => joke.joke !== currentJoke);
+  }
+  this.saveFavoriteJokes();
+}
+
+saveFavoriteJokes() {
+  this.jokeService.saveFavoriteJokes(this.favouriteJokes);
+}
+
+loadFavoriteJokes() {
+  this.favouriteJokes = this.jokeService.getFavoriteJokes();
+}
+
+deleteJoke(index: number): void {
+  this.favouriteJokes.splice(index, 1);
+  this.jokeService.saveFavoriteJokes(this.favouriteJokes);
 }
 }
--- JokeApp/src/app/joke-service.ts
+++ JokeApp/src/app/joke-service.ts
@@ -6,11 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class JokeService {
+
+  private favoriteJokesKey='favoriteJokes';
   private apiUrl = 'https://v2.jokeapi.dev/joke/Any?type=single&amount=10';
   constructor( private http:HttpClient) { }
 
   fetchJokes(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
+
+  saveFavoriteJokes(jokes: any[]) {
   
+    const jokesWithTimestamp = jokes.map(joke => ({
+      joke: joke.joke,
+      timestamp: new Date().toDateString() 
+    }));
+
+    localStorage.setItem(this.favoriteJokesKey, JSON.stringify(jokesWithTimestamp));
+  }
+
+  getFavoriteJokes(): any[] {
+    const jokesString = localStorage.getItem(this.favoriteJokesKey);
+    return jokesString ? JSON.parse(jokesString) : [];
+  }
 }
