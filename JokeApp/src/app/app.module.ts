8d41af9aa174a7cbc2a33ff705e10e42ebb993cd
FilipKramar <filip.kramar4637@gmail.com>
2024-02-16 20:27:29 +0100

    Added chat bubble and modal window

--- JokeApp/src/app/app.module.ts
+++ JokeApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { JokeComponent } from './joke-component/jokeComponent';
 import { HeaderComponent } from './header/header';
 import { HttpClientModule } from '@angular/common/http';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
